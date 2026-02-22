03341a0855d0dba0afd0d47698a666f4f5ce5b00
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-02-22 20:08:13 +0600

    added in review and rejected btn with js

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,7 +12,7 @@ let result = total;
 totalJob.innerText = result;
 totalElement.innerText = result;
 
-
+// allbtn setup
 
 const allBtn = document.getElementById("All-btn").addEventListener("click", function(){
 
@@ -35,7 +35,93 @@ const allBtn = document.getElementById("All-btn").addEventListener("click", func
 })
 
 
+// interviewone btn 
+let totalTwo = document.getElementById("total-two");
+let count = 0;
 
+let allBtnOfReview = document.querySelectorAll(".interviewOne");
+
+for(let btn of  allBtnOfReview){
+  
+  btn.addEventListener("click", function(){
+    count = count + 1;
+    totalTwo.innerText = count;
+  })
+  
+}
+
+
+
+// local storage set total number  
+
+
+
+
+// rejected btn
+ let totalThree = document.getElementById("total-three");
+ let allRejectedBtns = document.querySelectorAll(".Rejected-btn-one");
+
+ for(let btn of allRejectedBtns){
+         btn.addEventListener("click", function(){
+
+          count = count + 1;
+          totalThree.innerText = count
+
+         })
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let  interView = document.getElementById("interview");
+// let totalTwo = document.getElementById("total-two");
+// let totalCount = localStorage.getItem("total-two")
+// let count = 0;
+
+// const interViewOnebtn = document.getElementById("interviewOne").addEventListener("click", function(){
+
+//      count = count + 1;
+//      totalTwo.innerText = count;
+
+// })
+
+// interview btn 
+
+
+  
+
+
+
+// rejectedbtn
+
+// const totalThree = document.getElementById("total-three");
+// let reject = 0;
+    
+// const RejectedBtnOne = document.getElementById("Rejected-btn-one").addEventListener("click", function(){
+
+//    reject = reject + 1;
+
+// totalThree.innerText = reject;
+
+
+// })
 
 
 
